3b05bb752c34e7e2dc5b243bacf79755a3bd44da
Saurabh <jsaurabh1906@gmail.com>
2025-02-26 11:37:43 +0530

    feat: Notes APP
    This Notes App is built with:
    - React for the frontend
    - Redux Toolkit for state management
    - Tailwind CSS for styling
    - Appwrite as the backend for database

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./components/Header";
+import Notes from "./components/Notes";
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <>
+      <Header />
+      <Notes />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
